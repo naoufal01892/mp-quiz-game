0278f395fea98a01b8a7cb8e7830d934c01f3e58
NAOUFAL NAKHCHA <nakhchanaoufal@gmail.com>
2023-11-10 18:50:00 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,11 @@
-import csv 
+import csv
+import random
+
+csv_file_path = 'Book1.csv'
+
+with open(csv_file_path, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    csv_data = list(csv_reader) 
+    random_line = random.choice(csv_data)
+    first_item = random_line[0]
+    print(first_item)
