22bcda7c6f550afa357f24c01ca4576b9b4cfd75
NAOUFAL NAKHCHA <nakhchanaoufal@gmail.com>
2023-11-11 17:34:58 +0100

    sound

---
+++ sound/sound.py
@@ -0,0 +1,24 @@
+# importing required module
+from playsound import playsound
+from tkinter import*
+
+root = Tk() 
+root.title('GeeksforGeeks sound player') #giving the title for our window
+root.geometry("500x400")
+
+# making function 
+def play():
+	playsound('1.mp3')
+
+title=Label(root,text="GeeksforGeeks",bd=9,relief=GROOVE,
+			font=("times new roman",50,"bold"),bg="white",fg="green") 
+title.pack(side=TOP,fill=X) 
+
+
+play_button = Button(root, text="Play Song", font=("Helvetica", 32),
+					relief=GROOVE, command=play)
+play_button.pack(pady=20)
+
+info=Label(root,text="Click on the button above to play song ",
+		font=("times new roman",10,"bold")).pack(pady=20)
+root.mainloop()
